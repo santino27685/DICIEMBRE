4696c6a453259a47986f77e12f219ad4a247a2fb
unknown <randazzosantino3@gmail.com>
2024-12-10 20:48:51 -0300

    Navbar eliminada y modo oscuro se guarda en LocalStorage

--- theme.js
+++ theme.js
@@ -1,30 +1,65 @@
+function applyDarkMode() {
+    document.body.classList.add("dark-mode");
+    document.querySelectorAll(".input-group input").forEach(function (input) {
+        input.classList.add("dark-mode");
+    });
+    document.querySelectorAll(".btn").forEach(function (btn) {
+        btn.classList.add("dark-mode");
+    });
+    document.querySelectorAll(".login-box, .register-box").forEach(function (box) {
+        box.classList.add("dark-mode");
+    });
+    document.querySelectorAll(".welcome-text").forEach(function (text) {
+        text.classList.add("dark-mode");
+    });
+    document.querySelectorAll("p a").forEach(function (link) {
+        link.classList.add("dark-mode");
+    });
+    document.querySelectorAll(".theme-toggle a").forEach(function (link) {
+        link.classList.add("dark-mode");
+    });
+    document.querySelectorAll(".image-box img").forEach(function (img) {
+        img.classList.add("dark-mode");
+    });
+}
+
+function removeDarkMode() {
+    document.body.classList.remove("dark-mode");
+    document.querySelectorAll(".input-group input").forEach(function (input) {
+        input.classList.remove("dark-mode");
+    });
+    document.querySelectorAll(".btn").forEach(function (btn) {
+        btn.classList.remove("dark-mode");
+    });
+    document.querySelectorAll(".login-box, .register-box").forEach(function (box) {
+        box.classList.remove("dark-mode");
+    });
+    document.querySelectorAll(".welcome-text").forEach(function (text) {
+        text.classList.remove("dark-mode");
+    });
+    document.querySelectorAll("p a").forEach(function (link) {
+        link.classList.remove("dark-mode");
+    });
+    document.querySelectorAll(".theme-toggle a").forEach(function (link) {
+        link.classList.remove("dark-mode");
+    });
+    document.querySelectorAll(".image-box img").forEach(function (img) {
+        img.classList.remove("dark-mode");
+    });
+}
+
+if (localStorage.getItem("theme") === "dark") {
+    applyDarkMode();
+}
+
 document.querySelectorAll("#toggleTheme").forEach(function (toggle) {
     toggle.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
-        document.querySelector("nav").classList.toggle("dark-mode");
-        document.querySelectorAll(".input-group input").forEach(function (input) {
-            input.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll(".btn").forEach(function (btn) {
-            btn.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll("nav ul li a").forEach(function (link) {
-            link.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll(".login-box, .register-box").forEach(function (box) {
-            box.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll(".welcome-text").forEach(function (text) {
-            text.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll("p a").forEach(function (link) {
-            link.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll(".theme-toggle a").forEach(function (link) {
-            link.classList.toggle("dark-mode");
-        });
-        document.querySelectorAll(".image-box img").forEach(function (img) {
-            img.classList.toggle("dark-mode");
-        });
+        if (document.body.classList.contains("dark-mode")) {
+            removeDarkMode();
+            localStorage.setItem("theme", "light");
+        } else {
+            applyDarkMode();
+            localStorage.setItem("theme", "dark");
+        }
     });
 });
